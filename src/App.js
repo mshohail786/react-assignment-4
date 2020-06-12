e07fd97893d45defb7f924c386620160ada925ec
Muhammad Hasan <muh.hasan.git@gmail.com>
2020-06-12 19:02:49 +0500

    'react-classs-4'

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
+import React, { useState } from 'react';
+import {Message} from './message.js'
+import './App.css'
+export default function App() {
+  let [day, setDayfan] = useState(1)
+  let [night, setMynight] = useState(true)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ 
+ <div className={`box ${night ? 'light' :'dlight'} ` } >
+      
+    
+    {/* <div className={night ? 'light':'dlight',} > */}
+  {/* //  <div className = {night ? 'light' : ''}> */}
+    <h1>Good  {night ? 'Morning' :'Night'} </h1>
+      <Message mday={day}/>
+      <br></br>
+      
+      <button onClick={()=>setDayfan(++day)}>Increment</button>
+          <button onClick={()=>setDayfan(--day)} >Decrement</button>
+
+
+          <button onClick={()=>setMynight(!night)} >Day /Night</button>
     </div>
+    
   );
 }
 
-export default App;
+
+
+// export default App;
---
+++ src/message.js
@@ -0,0 +1,8 @@
+import React  from 'react' ;
+
+export function Message (props) {
+
+    return(
+        <h1>Counter {props.mday} </h1>
+    )
+}
